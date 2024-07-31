b5a9435e9e365d7809d35f6e8919845e420fe5ab
Vitalii-T-code <vatretiakov.w@gmail.com>
2024-07-31 15:03:39 +0500

    Add files via upload

---
+++ ProjectZeroGravitySphereScripts/ExplosionController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class ExplosionController : MonoBehaviour
+{
+    private const float explosionForce = 3000.0f;
+    private const float explosionRadius = 5.0f;
+
+    [SerializeField] private Transform explosionPoint;
+
+    private void Start()
+    {
+        ApplyExplosionForce();
+    }
+
+    private void ApplyExplosionForce()
+    {
+        Collider[] colliders = Physics.OverlapSphere(explosionPoint.position, explosionRadius);
+        foreach (Collider hit in colliders)
+        {
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.AddExplosionForce(explosionForce, explosionPoint.position, explosionRadius);
+            }
+        }
+    }
+}
---
+++ ProjectZeroGravitySphereScripts/ZeroGravityController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZeroGravityController : MonoBehaviour
+{
+    private const float sinkDuration = 1.0f; // Константа
+
+    [SerializeField] private float gravityOffDuration = 3.0f; // Сериализованное поле
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ball"))
+        {
+            StartCoroutine(DisableGravityTemporarily(other.gameObject));
+        }
+    }
+
+    private IEnumerator DisableGravityTemporarily(GameObject ball)
+    {
+        Rigidbody rb = ball.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.useGravity = false;
+
+            // Ожидание заданного времени
+            yield return new WaitForSeconds(gravityOffDuration);
+
+            rb.useGravity = true;
+        }
+    }
+}
