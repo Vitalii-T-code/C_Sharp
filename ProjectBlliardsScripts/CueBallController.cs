4c0d74bfaf7999da5b2e956fe66ecee775e01f8b
Vitalii-T-code <vatretiakov.w@gmail.com>
2024-07-31 15:04:00 +0500

    Add files via upload

---
+++ ProjectBlliardsScripts/CueBallController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class CueBallController : MonoBehaviour
+{
+    // Константы
+    private const float DefaultInitialForce = 5f;
+    private static readonly Vector3 InitialDirection = new Vector3(1, 0, 1).normalized;
+
+    // Сериализованные поля
+    [SerializeField]
+    private float initialForce = DefaultInitialForce;
+
+    // Методы жизненного цикла Unity
+    private void Start()
+    {
+        ApplyInitialForce();
+    }
+
+    // Публичные методы
+
+    // Все остальные методы
+    private void ApplyInitialForce()
+    {
+        Rigidbody rb = GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.AddForce(InitialDirection * initialForce, ForceMode.Impulse);
+        }
+    }
+
+    // Встроенные типы
+}
---
+++ ProjectBlliardsScripts/PocketController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PocketController : MonoBehaviour
+{
+    // Константы
+    private const float SinkDuration = 1.0f;
+    private static readonly Vector3 SinkOffset = new Vector3(0, -0.5f, 0);
+
+    // Методы жизненного цикла Unity
+    private void OnTriggerEnter(Collider other)
+    {
+        HandleBallCollision(other);
+    }
+
+    // Публичные методы
+
+    // Все остальные методы
+    private void HandleBallCollision(Collider other)
+    {
+        if (other.CompareTag("Ball"))
+        {
+            StartCoroutine(SinkBall(other.gameObject));
+        }
+    }
+
+    private IEnumerator SinkBall(GameObject ball)
+    {
+        Rigidbody rb = ball.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.isKinematic = true; // Отключаем физику для шара
+        }
+
+        Vector3 startPosition = ball.transform.position;
+        Vector3 endPosition = startPosition + SinkOffset; // Опускаем шар вниз
+
+        float elapsedTime = 0;
+
+        while (elapsedTime < SinkDuration)
+        {
+            ball.transform.position = Vector3.Lerp(startPosition, endPosition, elapsedTime / SinkDuration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        Destroy(ball); // Удаляем шар после анимации
+    }
+
+    // Встроенные типы
+}
