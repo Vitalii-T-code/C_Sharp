0909bf47aab222bbac5f01cfe8b95bd53eb30027
Vitalii-T-code <vatretiakov.w@gmail.com>
2024-07-31 07:43:50 +0500

    Add files via upload

---
+++ ProjectLabyrinthScripts/BallController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody _ballRigidbody; // Rigidbody шарика
+    [SerializeField] private float _forceMultiplier = 10f; // множитель силы для управления движением шарика
+
+    void Update()
+    {
+        HandleTouchInput();
+    }
+
+    private void HandleTouchInput()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Moved)
+            {
+                Vector3 forceDirection = new Vector3(touch.deltaPosition.x, 0, touch.deltaPosition.y).normalized;
+                _ballRigidbody.AddForce(forceDirection * _forceMultiplier);
+            }
+        }
+    }
+}
